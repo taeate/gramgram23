3b14872a50de4b4c48e30288b3fbe9cac84af8eb
taeate <yes3144@nate.com>
2023-04-07 16:18:21 +0900

    페이지 진입전에 로그인 여부 체크

--- src/main/java/com/ll/gramgram/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/ll/gramgram/boundedContext/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
     private final MemberService memberService;
 
+    @PreAuthorize("isAnonymous()")
     @GetMapping("/join")
     public String showJoin(){
         return "usr/member/join";
@@ -33,7 +35,7 @@ public class MemberController {
         @Size(min = 4, max = 30)
         private final String password;
     }
-
+    @PreAuthorize("isAnonymous()")
     @PostMapping("/join")
     public String join(@Valid JoinForm joinForm) {
         memberService.join(joinForm.getUsername(), joinForm.getPassword());
@@ -41,6 +43,7 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @PreAuthorize("isAnonymous()") // 로그인 안한사람만 들어올수있음
     @GetMapping("/login")
     public String showLogin(){
         return "usr/member/login";
